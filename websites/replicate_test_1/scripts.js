273fc01d5c3ce474e9824090ffaa732e5519627e
DinuDavidsohn <Dinu.Davidsohn@gmail.com>
2019-01-27 13:11:29 -0500

    changes to repl test 1 refactor js and rewrote images

--- websites/replicate_test_1/scripts.js
+++ websites/replicate_test_1/scripts.js
@@ -1,54 +1,42 @@
-function get_landscape() {
-    var input, images;
-    var images_found = 0
-
-    input = document.getElementById("searchbar").value
-    images = document.getElementsByClassName("image")
-
-    for (i = 0; i < images.length; i++) {
-        var image = images[i]
-        var attribute_list_raw = image.getAttribute("data-landscape")
-        var attribute_list = attribute_list_raw.split(",")
-        var attributeFound = false
-        for (j = 0; j < attribute_list.length; j++) {
-            if (input != attribute_list[j] && attributeFound == false) {
-                image.style.display = "none"
-            } else if (input == attribute_list[j]) {
-                image.style.display = "inline-block"
-                images_found++
-                attributeFound = true
-            }
-        }
-    }
-    if (images_found == 0) {
-        for (i = 0; i < images.length; i++) {
-            images[i].style.display = "inline-block"
-            
-        }
-    }
+function searchLandscapes() {
+  var input = document.getElementById("searchbar").value
+  applyFilter(input)
 }
-function select_landscape() {
-    var trait = document.getElementById("selectbar").value
-    var images = document.getElementsByClassName("image")
-    images_found = 0
+
+function selectLandscapes() {
+  var trait = document.getElementById("selectbar").value
+  applyFilter(trait)
+}
+
+function autocomplete_search() {
+  input = document.getElementById("autocompletebar").value
+  var traits = ["cave", "city", "coast", "desert", "forest", "lake", "mountains", "river", "temple", "tropical", "watefall"]
+}
+
+function applyFilter(query) {
+  var images_found = 0
+  var imagesContainer = document.getElementsByClassName("images")
+  var images = imagesContainer[0].children
+
+  for (i = 0; i < images.length; i++) {
+    var image = images[i]
+    var attribute_list_raw = image.getAttribute("data-landscape")
+    var attribute_list = attribute_list_raw.split(",")
+    var attributeFound = false
+    for (j = 0; j < attribute_list.length; j++) {
+      if (query != attribute_list[j] && attributeFound == false) {
+        image.style.display = "none"
+      } else if (query == attribute_list[j]) {
+        image.style.display = "inline-block"
+        images_found++
+        attributeFound = true
+      }
+    }
+  }
+  if (images_found == 0) {
     for (i = 0; i < images.length; i++) {
-        var image = images[i]
-        var attribute_list_raw = image.getAttribute("data-landscape")
-        var attribute_list = attribute_list_raw.split(",")
-        var attributeFound = false
-        for (j = 0; j < attribute_list.length; j++) {
-            if (trait != attribute_list[j] && attributeFound == false) {
-                image.style.display = "none"
-            } else if (trait == attribute_list[j]) {
-                image.style.display = "inline-block"
-                attributeFound = true
-                images_found++
-            }
-        }
+      images[i].style.display = "inline-block"
+
     }
-    if (images_found == 0) {
-        for (i = 0; i< images.length; i++) {
-            images[i].style.display = "inline-block"
-        }
-    }
+  }
 }
