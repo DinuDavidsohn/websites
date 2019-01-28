ee9968d0047816f047fde6da7dc22ff96761e306
DinuDavidsohn <Dinu.Davidsohn@gmail.com>
2019-01-27 20:16:01 -0500

    created 1st iteration rock-paper-scissors game in repl test 1

--- websites/replicate_test_1/scripts.js
+++ websites/replicate_test_1/scripts.js
@@ -81,4 +81,56 @@ function applyFilter(query) {
     }
   }
 }
+function runGame(query) {
+  var robotChoice = Math.floor((Math.random()*3)+1)
+  console.log(robotChoice)
+  switch(robotChoice) {
+    case 1:
+    document.getElementById("gameStart").innerHTML = "The computer chooses Rock!"
+    break
+    case 2:
+    document.getElementById("gameStart").innerHTML = "The computer chooses Paper!"
+    break
+    case 3:
+    document.getElementById("gameStart").innerHTML = "The computer chooses Scissors!"
+    break
+  }
+  switch(query) {
+    case "rock":  
+      query = 1
+      break
+    case "paper":
+      query = 2
+      break
+    case "scissors":
+      query = 3
+      break
+  }
+  if (robotChoice == query) {
+    document.getElementById("outcome").innerHTML = "You drew against the computer!"
+    playedGameIncrement()
+  }
+  else if (robotChoice == 2 && query == 3 || robotChoice == 3 && query == 1 || robotChoice == 1 && query == 2) {
+    document.getElementById("outcome").innerHTML = "You beat the computer!"
+    wonGameIncrement()
+    playedGameIncrement()
+
+  }
+  else if (robotChoice == 1 && query == 3 || robotChoice == 2 && query == 1 || robotChoice == 3 && query == 2) {
+    document.getElementById("outcome").innerHTML = "You lost to the computer!"
+    playedGameIncrement()
+  }
+}
+var gamesPlayed = 0
+var gamesWon = 0
+function playedGameIncrement() {
+  gamesPlayed++
+  document.getElementById("timesPlayed").innerHTML = "Times Played: " + gamesPlayed  
+}
+function wonGameIncrement() {
+  gamesWon++
+  document.getElementById("timesWon").innerHTML = "Times Won: " + gamesWon  
+
+
+}
 window.onload = startPosition();
